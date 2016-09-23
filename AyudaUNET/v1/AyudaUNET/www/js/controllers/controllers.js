e06d8aaf8f4791028c1b6272b83a23eb7b06d3c8
Javier Moreno <javiomoreno@gmail.com>
2016-09-22 22:32:28 -0400

    creacion del inicio

--- AyudaUNET/v1/AyudaUNET/www/js/controllers/controllers.js
+++ AyudaUNET/v1/AyudaUNET/www/js/controllers/controllers.js
@@ -223,48 +223,58 @@ angular.module('starter.controllers', [])
     }
   })
 
-  .controller('TablaCtrl', function($scope, $ionicPopup) {
+  .controller('TablaCtrl', function($scope, $ionicPopup, $ionicLoading) {
 
     $scope.showAlert = function(nota) {
 
-      var _tabla = new Array(100);
-      var _numero = 0;
-
-      for (var j = 0; j <= 100; j++)
-      {
-        if(j < 8)
-          _numero = 10;
-        else if(j == 17 || j == 18)
-          _numero = 20;
-        else if(j == 28 || j == 29)
-          _numero = 30;
-        else if(j == 39 || j == 40)
-          _numero = 40;
-        else if(j == 48 || j == 49)
-          _numero = 48;
-        else if(j == 54 || j == 55)
-          _numero = 53;
-        else if(j == 62 || j == 63)
-          _numero = 60;
-        else if(j == 73 || j == 74)
-          _numero = 70;
-        else if(j == 84 || j == 85)
-          _numero = 80;
-        else if(j > 94)
-          _numero = 90;
-        else
-          _numero ++;
-        _tabla[j] = _numero/10;
+      if(angular.isUndefined(nota) || nota == null || nota == ''){
+        $ionicLoading.show({template: 'Ingrese Nota para Calcular!', noBackdrop: true, duration: 3000});
       }
+      else {
+        if (nota > 100 || nota < 0) {
+          $ionicLoading.show({template: 'La  Nota debe estar entre 0 y 100!', noBackdrop: true, duration: 3000});
+        }
+        else {
+          var _tabla = new Array(100);
+          var _numero = 0;
 
-      var alertPopup = $ionicPopup.alert({
-        title: 'Tabla de Conversión!',
-        template: 'El Resultado es: '+_tabla[nota],
-      });
+          for (var j = 0; j <= 100; j++)
+          {
+            if(j < 8)
+              _numero = 10;
+            else if(j == 17 || j == 18)
+              _numero = 20;
+            else if(j == 28 || j == 29)
+              _numero = 30;
+            else if(j == 39 || j == 40)
+              _numero = 40;
+            else if(j == 48 || j == 49)
+              _numero = 48;
+            else if(j == 54 || j == 55)
+              _numero = 53;
+            else if(j == 62 || j == 63)
+              _numero = 60;
+            else if(j == 73 || j == 74)
+              _numero = 70;
+            else if(j == 84 || j == 85)
+              _numero = 80;
+            else if(j > 94)
+              _numero = 90;
+            else
+              _numero ++;
+            _tabla[j] = _numero/10;
+          }
 
-      alertPopup.then(function(res) {
-        console.log('close');
-      });
+          var alertPopup = $ionicPopup.alert({
+            title: 'Tabla de Conversión!',
+            template: 'El Resultado es: '+_tabla[nota],
+          });
+
+          alertPopup.then(function(res) {
+
+          });
+        }
+      }
     };
   })
 
@@ -539,7 +549,7 @@ angular.module('starter.controllers', [])
           }, function (err) {
             console.error(err);
           });
-          $state.go('app.guarda', {}, {reload: true, inherit: true, notify: true});
+          $state.go('app.guarda');
         }
       }
       else if($scope.nueva.cantParciales.value == 3){
