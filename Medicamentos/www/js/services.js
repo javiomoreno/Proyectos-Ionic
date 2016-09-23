22836099d1af9bf000720c453aeaf1d58eca4025
Javier Moreno <javiomoreno@gmail.com>
2016-09-23 08:51:29 -0400

    commit oficina

--- Medicamentos/www/js/services.js
+++ Medicamentos/www/js/services.js
@@ -8,27 +8,27 @@ angular.module('starter.services', [])
     id: 0,
     name: 'Ben Sparrow',
     lastText: 'You on your way?',
-    face: 'img/ben.png'
+    face: 'img/alarm_on.svg'
   }, {
     id: 1,
     name: 'Max Lynx',
     lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    face: 'img/alarm_on.svg'
   }, {
     id: 2,
     name: 'Adam Bradleyson',
     lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
+    face: 'img/alarm_on.svg'
   }, {
     id: 3,
     name: 'Perry Governor',
     lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
+    face: 'img/alarm_on.svg'
   }, {
     id: 4,
     name: 'Mike Harrington',
     lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    face: 'img/alarm_on.svg'
   }];
 
   return {
